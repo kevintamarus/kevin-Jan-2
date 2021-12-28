9d64adaa5f03daa66c83817ebf5f9c3663d60a49
Kevin Tamarus <kevintamarus@gmail.com>
2021-12-27 23:27:43 -0800

    typescript config and formatter fixed, app runs

---
+++ src/screens/Main/index.tsx
@@ -0,0 +1,8 @@
+import React, {FunctionComponent} from 'react';
+import {Text} from 'react-native';
+
+const Main: FunctionComponent = () => {
+  return <Text>Main Screen is Here</Text>;
+};
+
+export default Main;
